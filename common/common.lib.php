26d500cca69cc0afafef9112e416eef58caaabe5
sergets <sergets@ya.ru>
2013-02-22 00:36:30 +0000

    resolvePath - убраны логи

--- common/common.lib.php
+++ common/common.lib.php
@@ -20,25 +20,20 @@
             $baseURI = $SXMLParams['docroot'];
         }
         $baseURI = str_replace('\\', '/', $baseURI);
-        echo('1 > '.$baseURI.'<br>');
         if (!is_dir($baseURI)) {
             $baseURI = substr($baseURI, 0, strrpos($baseURI, '/'));
-            echo('2 > '.$baseURI.'<br>');
         }
         
         if (($s = strpos($path, '//'.$SXMLParams['host'])) !== false) {
             $path = substr($path, $s + strlen('//'.$SXMLParams['host'])); // [http:]//sergets.ru/dir/ -> /dir/
             if ($path == '') $path = '/';
-            echo('3 > '.$path.'<br>');
         } else if (strpos($path, '//') !== false) { 
             return false; // Другой сайт
         }
         if (strpos($path, '/') === 0) {
             $path = str_replace('\\', '/', $SXMLParams['docroot']).$path;
-            echo('4 > '.$path.'<br>');
         } else {
             $path = $baseURI.'/'.$path;
-            echo('5 > '.$path.'<br>');
         }
         if (strpos($path, '..') === 0 || strpos($path, '/..') === 0) {
             return false;
@@ -46,12 +41,9 @@
         $path = str_replace('/./', '/', $path);
         while (strpos($path, '/..') !== false) {
             $path = preg_replace('/\/([^\/]*[^\/\.][^\/]*)\/\.\./', '', $path);
-            echo('6 > '.$path.'<br>');
         }
 
-        echo('7 > '.$path.'<br>');
         if (strpos($path, str_replace('\\', '/', $SXMLParams['localroot'])) !== 0) {
-            echo('8 > '.str_replace('\\', '/', $SXMLParams['localroot']).' is at '.strpos($path, str_replace('\\', '/', $SXMLParams['localroot'])).'<br>');
             return false;
         } else {
             return $path;
