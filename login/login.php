c5f4482c5df41a07a6f3974ff88582573d29cf44
sergets <sergets@ya.ru>
2013-03-10 21:26:11 +0000

    Login/logout skeleton

--- login/login.php
+++ login/login.php
@@ -1,16 +1,27 @@
 <?
     require_once "login.lib.php";
+    require_once "../common/setup.php";
     
     session_start();
     
-    if (testLogin($_GET['u'])) { // TODO Нужна осмысленная проверка логина
+    if (isset($_SESSION['sxml:user'])) {
+        ?><<?='?'?>xml version="1.0"<?='?'?>>
+            <sxml:error xmlns:sxml="http://sergets.ru/sxml">Already logged in</sxml:error>
+        <?
+    } elseif (testLogin($_GET['u'])) { // TODO Нужна осмысленная проверка логина
         $user = $_GET['u'];
         if (!getUser($user)) {
             saveUser($user, getAdditionals($user));
         }
         $_SESSION['sxml:user'] = $user;
-        // TODO что отвечать
+        setcookie('userid_current', $user, 0, $SXMLParams['root']);
+        setcookie('userid_remember', $user, time() + 86400, $SXMLParams['root']);
+        ?><<?='?'?>xml version="1.0"<?='?'?>>
+            <sxml:ok xmlns:sxml="http://sergets.ru/sxml" sxml:user="<?=addslashes($user)?>>"/>
+        <?
     } else {
-        // TODO выдать ошибку
+        ?><<?='?'?>xml version="1.0"<?='?'?>>
+            <sxml:error xmlns:sxml="http://sergets.ru/sxml">Login sequence failed</sxml:error>
+        <?
     }
 ?>
--- login/logout.php
+++ login/logout.php
@@ -1,3 +1,14 @@
 <?
+    require_once "login.lib.php";
+    require_once "../common/setup.php";
     
+    session_start();
+    
+    unset($_SESSION['sxml:user']);
+    setcookie('userid_current', '', time() - 3600, $SXMLParams['root']);
+    setcookie('userid_remember', '', time() - 3600, $SXMLParams['root']);
+   
+    ?><<?='?'?>xml version="1.0"<?='?'?>>
+        <ok xmlns="http://sergets.ru/sxml"/>
+    <?
 ?>
