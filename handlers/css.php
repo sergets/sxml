abbbda7730e4bb94e0dbb95112fcc4b724d14570
sergets <sergets@ya.ru>
2013-07-10 09:16:21 +0000

    Относительные пути резолвятся и в заинклуженных CSS

--- handlers/css.php
+++ handlers/css.php
@@ -7,18 +7,22 @@
     require_once '../common/setup.php';
     require_once '../common/sxml.lib.php';
     
-    $css = file_get_contents($_SXML['file']);
-    $css = str_replace('url(//', 'url('.$SXMLParams['root'], $css);
-    preg_match_all('/\@import ("|url\()(.*)("|\));/', $css, $imports);
-    foreach ($imports[2] as $n => $file) {
-        $f = resolvePath($file, $_SXML['file']);
-        if (file_exists($f)) {
-            $css = str_replace($imports[0][$n], "/* ------- imported from ".$f." ----- */\n".file_get_contents($f)."\n/* --------- import ends -------- */\n\n", $css);
-        } else {
-            $css = str_replace($imports[0][$n], $imports[0][$n].' /* -- warning! '.$f.' not found */', $css);
+    function getCSSContent($theFile) {
+        global $SXMLParams;
+        $css = file_get_contents($theFile);
+        $css = str_replace('url(//', 'url('.$SXMLParams['root'], $css);
+        preg_match_all('/\@import ("|url\()(.*)("|\));/', $css, $imports);
+        foreach ($imports[2] as $n => $file) {
+            $f = resolvePath($file, $theFile);
+            if (file_exists($f)) {
+                $css = str_replace($imports[0][$n], "/* ------- imported from ".$f." ----- */\n".getCSSContent($f)."\n/* --------- import ends -------- */\n\n", $css);
+            } else {
+                $css = str_replace($imports[0][$n], $imports[0][$n].' /* -- warning! '.$f.' not found */', $css);
+            }
         }
+        return $css;
     }
     
     header('Content-type: text/css');
-    echo $css;
+    echo getCSSContent($_SXML['file']);
 ?>
