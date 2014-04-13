9ce41176bdded1ecabdebb9fd776efbd51ecce6c
sergets <sergets@ya.ru>
2014-04-13 15:15:21 +0000

    Names are properly saved

--- login/social-find.php
+++ login/social-find.php
@@ -6,7 +6,7 @@
     $find = $_GET['find'];
     
     if (isset($_SESSION['sxml:vk:accessToken'])) {
-        $res = requestJSON('https://api.vk.com/method/users.get?uids='.urlencode($find).'&fields=photo_50&access_token='.$_SESSION['sxml:vk:accessToken']);
+        $res = requestJSON('https://api.vk.com/method/users.get?uids='.urlencode($find).'&fields=photo_50,sex&access_token='.$_SESSION['sxml:vk:accessToken']);
         $response = isset($res['response']) ? $res['response'] : array();
         foreach ($response as $i => $r) {
             $result[] = array(
@@ -15,19 +15,15 @@
                 'name' => $r['first_name'] .' '. $r['last_name'],
                 'userpic' => $r['photo_50']
             );
-        }
-    
-        /*$res = requestJSON('https://api.vk.com/method/users.search?q='.urlencode($find).'&fields=photo_50&count=100&access_token='.$_SESSION['sxml:vk:accessToken']);
-        $response = isset($res['response']) ? $res['response'] : array();
-        array_shift($response);
-        foreach ($response as $i => $r) {
-            $result[] = array(
-                'type' => 'vk',
-                'user' => 'vk:' .  $r['uid'],
+            saveUser(array(
+                'provider' => 'vk',
+                'user' => 'vk:' . $r['uid'],
                 'name' => $r['first_name'] .' '. $r['last_name'],
+                'sex' => $r['sex'],
+                'link' => false,
                 'userpic' => $r['photo_50']
-            );
-        }*/
+            ));
+        }
     }
     
     print json_encode($result);
