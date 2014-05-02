5da1222af7db7ae4cb970baee56434f42b6239f8
Serge Ts <sergets@ya.ru>
2014-05-02 14:52:12 +0400

    Mock users for offline testing

--- login/login.php
+++ login/login.php
@@ -5,16 +5,24 @@
     session_start();
     
     $user = $_GET['u'];
+    $uarr = array(
+        'user' => 'test:' . $user,
+        'name' => 'Test user «'.$user.'»',
+        'provider' => 'test',
+        'sex' => 'm',
+        'link' => 'http://sergets.ru/',
+        'userpic' => 'https://avatars3.githubusercontent.com/u/1742292?s=50'
+    );
     
     if (isset($_SESSION['sxml:user'])) {
-        doLogin($user, array('user' => $user, 'name' => 'Test user "'.$user.'"') );
+        doLogin('test:'. $user, $uarr);
         ?><<?='?'?>xml version="1.0"<?='?'?>>
             <sxml:error xmlns:sxml="<?=$SXMLParams['ns']?>">Already logged in</sxml:error>
         <?
     } elseif (1) { // TODO Нужна осмысленная проверка логина
-        doLogin($user, array('user' => $user, 'name' => 'Test user "'.$user.'"') );
+        doLogin('test:'. $user, $uarr);
         ?><<?='?'?>xml version="1.0"<?='?'?>>
-            <sxml:ok xmlns:sxml="<?=$SXMLParams['ns']?>" sxml:user="<?=addslashes($user)?>>"/>
+            <sxml:ok xmlns:sxml="<?=$SXMLParams['ns']?>" sxml:user="test:<?=addslashes($user)?>"/>
         <?
     } else {
         ?><<?='?'?>xml version="1.0"<?='?'?>>
